a7b9fa2b312404276d716d2adfcf038362db09d3
omcgrath2014 <>
2020-10-08 21:33:10 -0700

    pause point

---
+++ Project_7b/Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\ETS Staff\Desktop\ALTSPACE\GITHUB\Proj_7_AltSpace_b\Project_7b\Project_7b\1579480109834830001_armadillos.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1579480109834830001.json
